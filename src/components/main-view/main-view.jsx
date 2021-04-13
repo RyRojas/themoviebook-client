42ac38f90654221b21628ce99967e6410569982d
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-04-12 17:32:18 -0700

    Styled main view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,44 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { RegistrationView } from '../registration-view/registration-view';
 
-export default class MainView extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            movies: [
-                { _id: 1, Title: 'Casablanca', Description: 'desc 1...', ImagePath: '...' },
-                { _id: 2, Title: 'Tokyo Story', Description: 'desc 2...', ImagePath: '...' },
-                { _id: 3, Title: 'In the Mood for Love', Description: 'desc 3...', ImagePath: '...' }
-            ],
-            selectedMovie: null
-        };
+
+import './main-view.scss';
+
+export default function MainView() {
+    const [ movies, setMovies ] = useState([]),
+        [ selectedMovie, setSelectedMovie ] = useState(null),
+        [ user, setUser ] = useState(null),
+        [ isRegistered, setRegistration ] = useState(true);
+
+    useEffect(() => {
+        axios.get('https://the-moviebook.herokuapp.com/movies')
+            .then(response => {
+                setMovies(response.data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    if (!isRegistered) {
+        return <RegistrationView 
+            onLogin={ user => setUser(user) }
+            onRegister={ (status) => setRegistration(status) }
+        />;
+    }    
+
+    if (!user) {
+        return <LoginView 
+            onLogin={ user => setUser(user) }
+            onRegister={ (status) => setRegistration(status) }
+        />;
     }
 
-    setSelectedMovie(newSelectedMovie) {
-        this.setState({
-            selectedMovie: newSelectedMovie
-        });
-    }
-
-    render() {
-        const { movies, selectedMovie } = this.state;
-
-        if (selectedMovie) {
-            return <MovieView movieData={ selectedMovie } 
-                onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>;
-        }
-
-        if (movies.length === 0) {
-            return <div className="main-view">The list is empty!</div>;
-        }
-
+    //Render Movie View if movie is selected
+    if (selectedMovie) {
         return (
-            <div className="main-view">
-                { movies.map((movie) => <MovieCard key={ movie._id } movieData={ movie } 
-                    onMovieClick={movie => { this.setSelectedMovie(movie) }} />)}
-            </div>
+            <Row className="movie-view justify-content-md-center">
+                <Col md={8}>
+                    <MovieView
+                        movieData={ selectedMovie }
+                        onBackClick={ newSelectedMovie => setSelectedMovie(newSelectedMovie) }
+                    />
+                </Col>
+            </Row>
         );
     }
+
+    if (movies.length === 0) {
+        return <div className="main-view" />
+    }
+
+    return(
+        <Row className="main-view row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 justify-content-center mx-5">
+            { movies.map(movie => (
+                <Col className="my-2 px-2" key={ movie._id}>
+                    <MovieCard movieData={ movie }
+                        onMovieClick={ movie => setSelectedMovie(movie) } />
+                </Col>
+            )
+            )}
+        </Row>
+    );
 }
+
+    
