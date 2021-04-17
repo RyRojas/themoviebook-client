8accb28ad4da899c1173975060b5e07bfd258af1
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-04-16 17:51:48 -0700

    Changed navbar style

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from 'react-router-dom';
 
 export function NavBar(props) {
     const { onLogout, userData } = props
     return (
-        <Navbar>
-            {console.log(userData)}
+        <Navbar variant="dark">
             <Navbar.Brand as={Link} to="/" className="fancy">theMovieBook</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse>
                 <Nav>
-                    <Nav.Link as={NavLink} to="/">Hi</Nav.Link>
                     <Nav.Link as={NavLink} to={`/users/${userData.Username}`}>Account</Nav.Link>
                     <Nav.Link onClick={ () => onLogout() }>Logout</Nav.Link>
                 </Nav>
