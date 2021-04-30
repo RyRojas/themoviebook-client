7980d83d9de7e68a0eeefc904c7135d2bad1945b
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-04-30 16:57:07 -0700

    Removed background from favorite icon

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -56,66 +56,66 @@ export function MovieView({ movieData, isFaved }) {
     let button;
 
     if (isFaved) {
-        button = <Button className="fav-button py-1 px-2" variant="dark" onClick={ () => handleUnfav(movieData._id) }>
-                <Image roundedCircle src={ FavIconFilled } alt="Remove from your favorites" />
-            </Button>
+        button = <Image src={ FavIconFilled } className="fav-button" onClick={ () => handleUnfav(movieData._id) } alt="Remove from your favorites" />
     } else {
-        button = <Button className="fav-button" variant="dark" onClick={ handleFav }>
-                <Image roundedCircle src={ FavIconEmpty} alt="Add to your favorites" />
-            </Button>
+        button = <Image src={ FavIconEmpty} className="fav-button" onClick={ handleFav } alt="Add to your favorites" />
     }
 
     //Render component
     return (
-        <Card className="movie-view-card mx-auto">
-            <Card.Img 
-                className="movie-poster"
-                variant="top"
-                src={ `/banner-${movieData.ImagePath}` }
-                alt={ movieData.Title } 
-            />
-            <Card.Body>
-                <Row className="justify-content-between px-3 mb-2 align-items-center">
-                    <div className="d-flex align-items-end">
-                        <Card.Title as={ 'h4' }>{ movieData.Title }</Card.Title>
-                        <Card.Title><small className="ml-2 mb-3">{`(${movieData.Year})`}</small></Card.Title>
-                    </div>
-                    {button}
-                </Row>
-                
+        <Row className="movie-view justify-content-md-center">
+            <Col sm={12} md={8} className="mx-auto">
+                <Card className="movie-view-card mx-auto">
+                    <Card.Img 
+                        className="movie-poster"
+                        variant="top"
+                        src={ `/banner-${movieData.ImagePath}` }
+                        alt={ movieData.Title } 
+                    />
+                    <Card.Body>
+                        <Row className="justify-content-between px-3 mb-2 align-items-center">
+                            <div className="d-flex align-items-end">
+                                <Card.Title as={ 'h4' }>{ movieData.Title }</Card.Title>
+                                <Card.Title><small className="ml-2 mb-3">{`(${movieData.Year})`}</small></Card.Title>
+                            </div>
+                            {button}
+                        </Row>
+                        
 
-                <Card.Text className="text-muted mb-2">Description</Card.Text>
-                <Card.Text className="mb-4">{ movieData.Description }</Card.Text>
+                        <Card.Text className="text-muted mb-2">Description</Card.Text>
+                        <Card.Text className="mb-4">{ movieData.Description }</Card.Text>
 
-                <hr />
-                <Row className="my-4">
-                    <Col sm={5}>
-                        <Card.Text className="text-muted mb-2">Directed by</Card.Text>
-                        <Link to={`/directors/${movieData.Director.Name}`}>
-                            <Button variant="link" className="pl-0 mb-2">{ movieData.Director.Name }</Button>
-                        </Link>
-                    </Col>
-                    <Col sm={5}>
-                        <Card.Text className="text-muted mb-2">Genre</Card.Text>
-                        <ListGroup horizontal>
-                            { movieData.Genre.map( genre => {
-                                return(
-                                    <Link to={`/genres/${genre.Name}`} key={genre.Name}>
-                                        <Button className="pl-0" variant="link">
-                                            { genre.Name }
-                                        </Button>
-                                    </Link>  
-                                )}
-                            )}
-                        </ListGroup>
-                    </Col>
-                </Row>
-                <hr className="mb-4"/>
+                        <hr />
+                        <Row className="my-4">
+                            <Col sm={5}>
+                                <Card.Text className="text-muted mb-2">Directed by</Card.Text>
+                                <Link to={`/directors/${movieData.Director.Name}`}>
+                                    <Button variant="link" className="pl-0 mb-2">{ movieData.Director.Name }</Button>
+                                </Link>
+                            </Col>
+                            <Col sm={5}>
+                                <Card.Text className="text-muted mb-2">Genre</Card.Text>
+                                <ListGroup horizontal>
+                                    { movieData.Genre.map( genre => {
+                                        return(
+                                            <Link to={`/genres/${genre.Name}`} key={genre.Name}>
+                                                <Button className="pl-0" variant="link">
+                                                    { genre.Name }
+                                                </Button>
+                                            </Link>  
+                                        )}
+                                    )}
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                        <hr className="mb-4"/>
 
-                <Button onClick={ () => history.goBack() }>Back</Button>
-            
-            </Card.Body>
-        </Card>
+                        <Button onClick={ () => history.goBack() }>Back</Button>
+                    
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
     );
 }
 
