54c96e46d596dc6b5c8c6aab6a055b84876f0acb
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-04-30 16:54:23 -0700

    Added logo within search bar

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -12,11 +12,19 @@ import SearchIcon from '../../assets/icons/search.svg';
 export function VisibilityFilterInput({ visibilityFilter }) {
     const dispatch = useDispatch();
 
-    return (<Form.Control className="search-bar mx-auto"
-        onChange={ e => dispatch(setFilter(e.target.value))}
-        value={ visibilityFilter }
-        placeholder="Search by title"
-    />);
+    return (
+        <div className="search-bar mx-auto w-50 d-flex pl-2">
+            <span className="align-self-center">
+                <img src={SearchIcon} aria-hidden="true" className="search-bar__icon" />
+            </span>
+            <Form.Control
+                className="search-bar__input shadow-none"
+                onChange={ e => dispatch(setFilter(e.target.value))}
+                value={ visibilityFilter }
+                placeholder="Search by title"
+            />
+        </div>
+    );
 }
 
 VisibilityFilterInput.propTypes = {
